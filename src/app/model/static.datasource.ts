df9a074e62d9f70caae85860bba778cf1137e961
ayush-csulb <ayush.vij@student.csulb.edu>
2020-05-07 03:38:16 -0700

    Added Admin Button

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -1,4 +1,3 @@
-  
 import { Injectable } from '@angular/core'
 import { Product } from './product.model'
 import { Observable, from } from 'rxjs'
@@ -7,30 +6,29 @@ import { Order } from './order.model'
 @Injectable()
 export class StaticDataSource {
   private products: Product[] = [
-    new Product(1, 'Product 1', 'Category 1', 'Product 1 (Category 1)', 100),
-    new Product(2, 'Product 2', 'Category 1', 'Product 2 (Category 1)', 100),
-    new Product(3, 'Product 3', 'Category 1', 'Product 3 (Category 1)', 100),
-    new Product(4, 'Product 4', 'Category 1', 'Product 4 (Category 1)', 100),
-    new Product(5, 'Product 5', 'Category 1', 'Product 5 (Category 1)', 100),
-    new Product(6, 'Product 6', 'Category 2', 'Product 6 (Category 2)', 100),
-    new Product(7, 'Product 7', 'Category 2', 'Product 7 (Category 2)', 100),
-    new Product(8, 'Product 8', 'Category 2', 'Product 8 (Category 2)', 100),
-    new Product(9, 'Product 9', 'Category 2', 'Product 9 (Category 2)', 100),
-    new Product(10, 'Product 10', 'Category 2', 'Product 10 (Category 2)', 100),
-    new Product(11, 'Product 11', 'Category 3', 'Product 11 (Category 3)', 100),
-    new Product(12, 'Product 12', 'Category 3', 'Product 12 (Category 3)', 100),
-    new Product(13, 'Product 13', 'Category 3', 'Product 13 (Category 3)', 100),
-    new Product(14, 'Product 14', 'Category 3', 'Product 14 (Category 3)', 100),
-    new Product(15, 'Product 15', 'Category 3', 'Product 15 (Category 3)', 100),
+    new Product(1, 'Cricket Bat', 'Cricket', 'Cricket Bat (Cricket)', 100),
+    new Product(2, 'Cricket Bowl', 'Cricket', 'Cricket Bowl (Cricket)', 70),
+    new Product(3, 'Soccer Ball', 'Soccer', 'Soccer Ball (Soccer)', 120),
+    new Product(4, 'Tennis Racket', 'Tennis', 'Tennis Racket (Tennis)', 150),
+    new Product(5, 'Swimming Cap', 'Swimming', 'Swimming Cap (Swimming)', 10),
+    new Product(6, 'Swimming Costume', 'Swimming', 'Swimming Costume (Swimming)', 30),
+    new Product(7, 'Chess Board', 'Indoor Games', 'Chess Board (Category 2)', 20),
+    new Product(8, 'Chess Warriors', 'Indoor Games', 'Chess Warriors (Indoor Games)', 5),
+    new Product(9, 'Board Games', 'Indoor Games', 'Board Games (Indoor Games)', 10),
+    new Product(10, 'Table Tennis Table', 'Indoor Games', 'Table Tennis Table (Indoor Games)', 200),
+    new Product(11, 'Table Tennis Ball', 'Indoor Games', 'Table Tennis Ball (Indoor Games)', 10),
+    new Product(12, 'Table Tennis Racket', 'Indoor Games', 'Table Tennis Racket (Indoor Games)', 40),
+    new Product(13, 'Handball', 'Handball', 'Handball (Handball)', 100),
+    new Product(14, 'Shoes', 'Misc', 'Shoes (Misc)', 50),
+    new Product(15, 'Stumps', 'Cricket', 'Stumps (Cricket)', 40),
   ]
 
   getProducts(): Observable<Product[]> {
     return from([this.products])
-
-}
+  }
 
   saveOrder(order: Order): Observable<Order> {
     console.log(JSON.stringify(order))
     return from([order])
-}
+  }
 }
